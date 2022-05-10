817d94831357fce25b2a0ae147b11cb1ec825f2b
Pablo Coronado <jospabcor@hotmail.com>
2022-05-09 21:22:52 -0700

    feat: modals

--- src/alumnos/alumnos.js
+++ src/alumnos/alumnos.js
@@ -1,8 +1,13 @@
+function init(){
+  
+  refreshList();
+  
+}
 
 
-
-
-  clearTable();
+ function refreshList(){
+ clearTable();
+ 
   let t = document.querySelector('#alumnoRow'),
   td = t.content.querySelectorAll("td");
   let tb = document.querySelector("tbody");
@@ -11,16 +16,37 @@
   for(let i = 0; i < 100; i++){
   td[0].textContent = "1235646565";
   td[1].textContent = "Stuff";
-  td[2].textContent = "Stuff";
-  td[3].textContent = "Stuff";
+  td[2].textContent = "coro";
+  td[3].textContent = "22";
   
 
   clone = document.importNode(t.content, true);
   tb.appendChild(clone);
-
+  
+}
+selectedRow();
 }
 
 
+function selectedRow(){
+
+  let index,
+  table = document.querySelector('.table');
+
+
+for(let i = 0; i < table.rows.length; i++){
+  table.rows[i].onclick = function(){
+    if(typeof index !== 'undefined'){
+      table.rows[index].classList.remove('selected');
+    }
+    
+    index = this.rowIndex;
+    this.classList.toggle("selected");
+    console.log(table.rows[index].cells);
+  }
+
+}
+}
 
 
 function clearTable(){
@@ -31,8 +57,51 @@ for (let i = tableHeaderRowCount; i < rowCount; i++) {
     table.deleteRow(tableHeaderRowCount);
 }
 }
-  
-  
-  
 
 
+
+
+
+
+init();
+
+ 
+// buttons //
+
+// add button //
+  let modalAdd = document.querySelector('#modal_containerAdd');
+  let  btnAdd = document.querySelector('.add');
+
+  btnAdd.addEventListener('click', () => {
+  
+  modalAdd.classList.add('show');
+});
+
+
+// remove button //
+let modalRemove = document.querySelector('#modal_containerRemove');
+  let btnRemove = document.querySelector('.rmv');
+
+  btnRemove.addEventListener('click', () => {
+  
+    modalRemove.classList.add('show');
+  });
+
+  // update button //
+
+  let modalUpdate = document.querySelector('#modal_containerUpdate');
+  let btnUpdate = document.querySelector('.updt');
+
+  btnUpdate.addEventListener('click', () => {
+    
+      modalUpdate.classList.add('show');
+  });
+
+  // show button //
+  let modalShow = document.querySelector('#modal_containerShow');
+  let btnShow = document.querySelector('.shw');
+
+  btnShow.addEventListener('click', () => {
+      
+        modalShow.classList.add('show');
+    });
