c3f0cdd5c9eef30bb3bbe0933e050f2c43b21a01
JAFB321 <jafb321@gmail.com>
2022-05-11 23:57:55 -0700

    fix: firebase app exports

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,18 +1,14 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore';
 import config from "./config.js";
 
 // Initialize Firebase
 const firebaseApp = initializeApp(config);
-const db = getFirestore(firebaseApp);
 
-async function getCities(db) {
-    const citiesCol = collection(db, 'cities');
-    const citySnapshot = await getDocs(citiesCol);
-    const cityList = citySnapshot.docs.map(doc => doc.data());
-    return cityList;
-  }
+// Init services
+const firebaseDB = getFirestore(firebaseApp);
 
-  
-
-export default firebaseApp;
+export {
+  firebaseApp,
+  firebaseDB
+};
